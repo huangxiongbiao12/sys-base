67d43736a680b8d36c3d571eee44e7e04ba1a74b
huangxb <626551861@qq.com>
2021-05-29 20:24:58 +0800

    项目初始化  代码生成工具  完善

--- manager/src/main/java/com/rm/manager/ManagerApplication.java
+++ manager/src/main/java/com/rm/manager/ManagerApplication.java
@@ -3,9 +3,8 @@ package com.rm.manager;
 import com.rm.common.generator.annotation.EnableCodeGenerate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableCodeGenerate
 public class ManagerApplication {
 
