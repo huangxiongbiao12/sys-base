be80c806feab660107c3db9211855bc74256c1ca
huangxb <626551861@qq.com>
2021-05-31 11:03:10 +0800

    项目初始化  权限校验  缓存实现处理

--- manager/src/main/java/com/rm/manager/ManagerApplication.java
+++ manager/src/main/java/com/rm/manager/ManagerApplication.java
@@ -2,12 +2,14 @@ package com.rm.manager;
 
 import com.rm.common.generator.annotation.EnableCodeGenerate;
 import com.rm.common.web.annotation.EnableWebConfig;
+import com.rm.common.web.security.annotation.EnableSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableCodeGenerate
 @EnableWebConfig
+@EnableSecurity
 public class ManagerApplication {
 
     public static void main(String[] args) {
