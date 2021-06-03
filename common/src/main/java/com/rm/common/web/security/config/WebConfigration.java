8f0ebc48c25769c8e74b7788d5e8ebbff73b5681
huangxb <626551861@qq.com>
2021-06-03 11:00:01 +0800

    时间格式统一处理

--- common/src/main/java/com/rm/common/web/security/config/WebConfigration.java
+++ common/src/main/java/com/rm/common/web/security/config/WebConfigration.java
@@ -1,12 +1,20 @@
 package com.rm.common.web.security.config;
 
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.rm.common.web.security.interceptor.AuthInterceptor;
 import com.rm.common.web.security.token.Token;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Administrator on 2017/8/16.
@@ -14,6 +22,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class WebConfigration extends WebMvcConfigurerAdapter {
+
+    /**
+     *  返回LocalDateTime值（去掉LocalDateTime中的T）
+     */
+    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
+    private String pattern;
 
     @Autowired
     private AuthInterceptor authInterceptor;
@@ -41,6 +55,38 @@ public class WebConfigration extends WebMvcConfigurerAdapter {
                     .maxAge(3600);
         }
     }
+
+    /**
+     * 接收前端datetime参数
+     * @return
+     */
+    @Bean
+    public Converter<String, LocalDateTime> LocalDateTimeConvert() {
+        return new Converter<String, LocalDateTime>() {
+            @Override
+            public LocalDateTime convert(String source) {
+                DateTimeFormatter df = DateTimeFormatter.ofPattern(pattern);
+                LocalDateTime date = null;
+                try {
+                    date = LocalDateTime.parse((String) source,df);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return date;
+            }
+        };
+    }
+
+    @Bean
+    public LocalDateTimeSerializer localDateTimeDeserializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
+    }
+
 
 
 }
