42635945a39c6b506b6d60fb41842415ebb0e34e
huangxb <626551861@qq.com>
2021-08-04 11:23:15 +0800

    防止sql注入

--- common/src/main/java/com/rm/common/jooq/ConditionUtils.java
+++ common/src/main/java/com/rm/common/jooq/ConditionUtils.java
@@ -1,6 +1,7 @@
 package com.rm.common.jooq;
 
 import com.rm.common.utils.ReflectUtil;
+import com.rm.common.web.response.ResponseEnum;
 import org.jooq.*;
 import org.jooq.impl.DSL;
 import org.springframework.util.StringUtils;
@@ -26,6 +27,8 @@ public class ConditionUtils {
             Set<String> keys = map.keySet();
             String orderKey = null;
             for (String key : keys) {
+                checkInjSql(key);
+                checkInjSql(map.get(key).toString());
                 // 排序跳过
                 if (key.startsWith(ORDER)) {
                     orderKey = key;
@@ -65,5 +68,26 @@ public class ConditionUtils {
         }
         return selectConditionStep;
     }
+
+
+    /**
+     * 检查sql注入
+     *
+     * @param str
+     * @return
+     */
+    public static void checkInjSql(String str) {
+        String flagStr = "`|'|and |and(|or |or(|exec|insert|select|delete|update|truncate |truncate(|char|declare |;|,";
+        //这里的东西还可以自己添加
+        String[] flags = flagStr.split("\\|");
+        for (int i = 0; i < flags.length; i++) {
+            // 含有特殊字符的处理
+            if (str.contains(flags[i])) {
+                throw ResponseEnum.SYSTEM_ERROR.newInstance("存在sql注入的风险: " + str);
+
+            }
+        }
+    }
+
 
 }
