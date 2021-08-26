a56e09c41135247eca946265eb0ef241d5a2e1d4
huangxb <626551861@qq.com>
2021-08-26 16:50:08 +0800

    日志输出结果处理优化

--- common/src/main/java/com/rm/common/web/annotation/EnableWebAutoLog.java
+++ common/src/main/java/com/rm/common/web/annotation/EnableWebAutoLog.java
@@ -14,6 +14,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Import({ResponseHandler.class})
-public @interface EnableWebConfig {
+@Import({LoggingFilter.class})
+public @interface EnableWebAutoLog {
 }
